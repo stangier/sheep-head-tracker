a310e967ed7aa6cf73be2256579875e5c1ee34a7
Lorenz Stangier <github@lorenzstangier.de>
2024-12-25 22:37:08 +0100

    Add modal functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html
+from dash import Dash, html, Input, Output, State, ctx
 import dash_bootstrap_components as dbc
 
 app = Dash(external_stylesheets=[dbc.themes.DARKLY])
@@ -27,8 +27,51 @@ table_points = [dbc.Table(table_header + table_body, bordered=True, id="tbl-poin
 div_player_buttons = html.Div(player_buttons + table_points, className="div-player-buttons")
 
 
-div_main = html.Div([div_player_buttons])
+modal = html.Div(
+    [
+        dbc.Modal(
+            [
+                dbc.ModalHeader(dbc.ModalTitle("Player Information")),
+                dbc.ModalBody(id="modal-body"),
+                dbc.ModalFooter(
+                    dbc.Button("Close", id="close-modal", className="ml-auto")
+                ),
+            ],
+            id="modal",
+            is_open=False,
+        )
+    ]
+)
+
+div_main = html.Div([div_player_buttons, modal])
 app.layout = div_main
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.callback(
+    Output("modal", "is_open"),
+    Output("modal-body", "children"),
+    Input("btn-player-1", "n_clicks"),
+    Input("btn-player-2", "n_clicks"),
+    Input("btn-player-3", "n_clicks"),
+    Input("btn-player-4", "n_clicks"),
+    Input("close-modal", "n_clicks"),
+    State("modal", "is_open"),
+)
+def toggle_modal(n1, n2, n3, n4, n_close, is_open):
+    if not ctx.triggered:
+        return is_open, ""
+
+    button_id = ctx.triggered[0]["prop_id"].split(".")[0]
+
+    if button_id == "close-modal":
+        return not is_open, ""
+
+    player_map = {
+        "btn-player-1": "Player One",
+        "btn-player-2": "Player Two",
+        "btn-player-3": "Player Three",
+        "btn-player-4": "Player Four",
+    }
+
+    return not is_open, f"Action originated from {player_map[button_id]}"
+
+app.run(debug=True)
